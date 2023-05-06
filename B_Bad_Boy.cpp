4376658c9295cbedec6decc2d17767d7ebfcc83f
gengyou <resisttempetation23@gmail.com>
2023-05-06 14:35:31 +0800

    restore

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <functional> // std::minus
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric> // std::accumulate
+#include <set>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int c = 0; c < t; ++c)
+    {
+
+        int h, w, x, y;
+
+        cin >> h >> w >> x >> y;
+
+        cout << 1 << " " << 1 << " " << h << " " << w;
+
+        cout << endl;
+    }
+
+    return 0;
+}
