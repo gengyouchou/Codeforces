dfb3c533b11acecb34b3e199376a2d145ba2b435
GengYou Zhou (周庚佑) <GengYouZhou@ami.com>
2023-05-09 15:16:46 +0800

    imp: C_Double_ended_Strings

--- C_Double_ended_Strings.cpp
+++ C_Double_ended_Strings.cpp
@@ -13,59 +13,6 @@
 
 using namespace std;
 
-int memo[22][22][22][22];
-
-int dfs(string &s, string &t, int si, int sj, int ti, int tj)
-{
-
-    if (ti > tj && si > sj)
-    {
-        return 0;
-    }
-
-    if (ti == tj && si == sj)
-    {
-        if (s[si] != t[ti])
-        {
-            return 2;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-
-    if (sj < 0 || tj < 0 || si >= s.size() || ti >= t.size())
-    {
-        return INT_MAX / 2;
-    }
-
-    if (memo[si][sj][ti][tj] != -1)
-    {
-        return memo[si][sj][ti][tj];
-    }
-
-    string x = s.substr(si, sj - si + 1);
-    string y = t.substr(ti, tj - ti + 1);
-
-    if (x == y)
-    {
-        return 0;
-    }
-
-    if (x == "" || y == "")
-    {
-        return INT_MAX / 2;
-    }
-
-    int a = 1 + dfs(s, t, si + 1, sj, ti, tj);
-    int b = 1 + dfs(s, t, si, sj - 1, ti, tj);
-    int c = 1 + dfs(s, t, si, sj, ti + 1, tj);
-    int d = 1 + dfs(s, t, si, sj, ti, tj - 1);
-
-    return memo[si][sj][ti][tj] = min(a, min(b, min(c, d)));
-}
-
 int main()
 {
 
@@ -78,9 +25,26 @@ int main()
 
         cin >> s >> q;
 
-        memset(memo, -1, sizeof(memo));
+        int n = s.size();
+        int m = q.size();
 
-        cout << dfs(s, q, 0, s.size() - 1, 0, q.size() - 1);
+        int ans = 0;
+
+        for (int len = 1; len <= min(n, m); ++len)
+        {
+            for (int i = 0; i + len <= n; ++i)
+            {
+                for (int j = 0; j + len <= m; ++j)
+                {
+                    if (s.substr(i, len) == q.substr(j, len))
+                    {
+                        ans = max(ans, len);
+                    }
+                }
+            }
+        }
+
+        cout << n + m - 2 * ans;
         cout << endl;
     }
 
