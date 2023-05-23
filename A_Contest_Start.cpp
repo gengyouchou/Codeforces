fde057b8522ea24292326ef259c4a7a53fe10e5a
gengyouchou <resisttempetation23@gmail.com>
2023-05-24 01:39:02 +0800

    too hard

---
+++ A_Contest_Start.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <functional> // std::minus
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric> // std::accumulate
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int c = 0; c < t; ++c)
+    {
+
+        int n = 0, x=0, t=0;
+
+        cin >> n >> x >> t;
+
+        //(0, 5), (2, 7) (4, 9) (6, 11)
+        //    2    2      1         
+
+
+
+        cout << endl;
+    }
+
+    return 0;
+}
