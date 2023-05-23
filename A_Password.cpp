03245c9b1faba3ab33390c8dc3436eacc5a66be5
GengYou Zhou (周庚佑) <GengYouZhou@ami.com>
2023-05-23 18:55:16 +0800

    combinatorics

---
+++ A_Password.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <functional> // std::minus
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric> // std::accumulate
+#include <set>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int c = 0; c < t; ++c)
+    {
+
+        int n = 0;
+
+        cin >> n;
+
+        vector<int> nums(n, 0);
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> nums[i];
+        }
+
+        long long ans = (10 - n) * (10 - n - 1) * 3;
+
+        cout << ans;
+
+        cout << endl;
+    }
+
+    return 0;
+}
