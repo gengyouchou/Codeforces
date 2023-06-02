eeac76831849c224195153fd06ba9ed6aca2f347
GengYou Zhou (周庚佑) <GengYouZhou@ami.com>
2023-06-02 19:18:42 +0800

    panding

---
+++ B_Fair_Numbers.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional> // std::minus
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric> // std::accumulate
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve()
+{
+    long long n;
+    cin >> n;
+
+    for (long long begin = n; begin <= LLONG_MAX; ++begin)
+    {
+        long long cur = begin;
+
+        while (cur > 0)
+        {
+            long long digit = cur % 10;
+
+            if (digit != 0 && begin % digit != 0)
+            {
+                break;
+            }
+
+            cur /= 10;
+        }
+
+        if (cur == 0)
+        {
+            cout << begin;
+            return;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 0;
+
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+    {
+        solve();
+
+        // if (solve())
+        // {
+        //     cout << "YES";
+        // }
+        // else
+        // {
+        //     cout << "NO";
+        // }
+
+        cout << endl;
+    }
+}
---
+++ B_Points_on_Plane.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional> // std::minus
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric> // std::accumulate
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve()
+{
+    long long n;
+    cin >> n;
+
+    long long m = (long long)sqrtl(n);
+
+    if (m * m == n)
+    {
+        cout << m - 1;
+    }
+    else
+    {
+        cout << m;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 0;
+
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+    {
+        solve();
+
+        // if (solve())
+        // {
+        //     cout << "YES";
+        // }
+        // else
+        // {
+        //     cout << "NO";
+        // }
+
+        cout << endl;
+    }
+}
