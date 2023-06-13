5532bcc8475c82cdd79afa4f8374224dfc177ca2
GengYou Zhou (周庚佑) <GengYouZhou@ami.com>
2023-06-13 19:17:24 +0800

    constructive algorithms

---
+++ C_Boats_Competition.cpp
@@ -0,0 +1,101 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional> // std::minus
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric> // std::accumulate
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+const ll N = 1'000'000'000'000L;
+
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (auto &x : vec)
+    {
+        cin >> x;
+    }
+
+    sort(vec.begin(), vec.end());
+
+    int ans = 0;
+
+    for (int sum = 2; sum <= 2 * n; ++sum)
+    {
+        int i = 0, j = n - 1;
+        int count = 0;
+
+        while (i < j)
+        {
+            int cur = vec[i] + vec[j];
+
+            if (cur > sum)
+            {
+                --j;
+            }
+            else if (cur < sum)
+            {
+                ++i;
+            }
+            else
+            {
+                ++count;
+                ++i;
+                --j;
+            }
+        }
+
+        ans = max(ans, count);
+    }
+
+    cout << ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 0;
+
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+    {
+        solve();
+
+        // if (solve())
+        // {
+        //     cout << "YES";
+        // }
+        // else
+        // {
+        //     cout << "NO";
+        // }
+
+        cout << endl;
+    }
+}
