1ef19ae8d8b2971a56aa2507bfc40bfecd9c39d7
GengYou Zhou (周庚佑) <GengYouZhou@ami.com>
2023-06-16 19:45:33 +0800

    classical construction problem

---
+++ A_Yaroslav_and_Permutations.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional> // std::minus
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric> // std::accumulate
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+const ll N = 1'000'000'000'000L;
+
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+bool solve()
+{
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (auto &x : vec)
+    {
+        cin >> x;
+    }
+
+    unordered_map<int, int> m;
+
+    for (auto &x : vec)
+    {
+        ++m[x];
+    }
+
+    for (auto [val, freq] : m)
+    {
+        if (freq > (n + 1) / 2)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    if (solve())
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
+    cout << endl;
+}
