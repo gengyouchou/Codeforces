545581641ef50208036371802bb441dd47425e97
GengYou Zhou (周庚佑) <GengYouZhou@ami.com>
2023-06-20 19:44:45 +0800

    panding

---
+++ C_Kill_the_Monster.cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional> // std::minus
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric> // std::accumulate
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+const ll N = 1'000'000'000'000L;
+
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+ll M = 1e9 + 7;
+
+bool dfs(int hc, int dc, int hm, int dm)
+{
+}
+
+bool solve()
+{
+    ll hc, dc, hm, dm;
+    cin >> hc >> dc >> hm >> dm;
+
+    ll coins, weapon, attack;
+    cin >> coins >> weapon >> attack;
+
+    while (hm > 0)
+    {
+        hm -= dc;
+        hc -= dm;
+
+        if (hc < 0 && coins == 0)
+        {
+            return false;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 0;
+
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+    {
+        solve();
+
+        // if (solve())
+        // {
+        //     cout << "YES";
+        // }
+        // else
+        // {
+        //     cout << "NO";
+        // }
+
+        cout << endl;
+    }
+}
