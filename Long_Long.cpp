377b8cd0a97f1cac7837a9d66b6b8f35f512f944
gengyouchou <resisttempetation23@gmail.com>
2023-06-21 01:38:56 +0800

    Codeforces Round 881 (Div. 3)

---
+++ Long_Long.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <functional> // std::minus
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric> // std::accumulate
+#include <queue>
+#include <set>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+const ll N = 1'000'000'000'000L;
+
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+ll M = 1e9 + 7;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<ll> vec(n);
+    ll sum = 0;
+    for (auto &x : vec)
+    {
+        cin >> x;
+        sum += ab(x);
+    }
+
+    int j = 0;
+
+    int group = 0;
+
+    for (int i = 0; i < n;)
+    {
+        if (vec[i] == 0)
+        {
+            ++i;
+            continue;
+        }
+
+        j = i;
+
+        while (j < n && vec[j] <= 0)
+        {
+            ++j;
+        }
+
+        if (i == j)
+        {
+            ++i;
+        }
+        else
+        {
+            ++group;
+            i = j;
+        }
+    }
+
+    cout << sum << " " << group;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 0;
+
+    cin >> t;
+
+    for (int i = 0; i < t; ++i)
+    {
+        solve();
+
+        // if (solve())
+        // {
+        //     cout << "YES";
+        // }
+        // else
+        // {
+        //     cout << "NO";
+        // }
+
+        cout << endl;
+    }
+}
