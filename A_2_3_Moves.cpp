a27d0cbadb474f4d516162bf6d9992e7d2d46315
gengyouchou <resisttempetation23@gmail.com>
2024-02-24 08:50:38 +0800

    good math problem

--- A_2_3_Moves.cpp
+++ A_2_3_Moves.cpp
@@ -1,41 +1,83 @@
 #include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
 #include <functional> // std::minus
 #include <iostream>
 #include <map>
 #include <math.h>
 #include <numeric> // std::accumulate
+#include <queue>
 #include <set>
+#include <stack>
 #include <stdio.h>
 #include <string.h>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
+
+// setprecision example
+#include <iomanip> // std::setprecision
 
 using namespace std;
 
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+
+const ll N = 1'000'000'000'000L;
+
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+ll M = 1e9 + 7;
+
+// int dp[1001][26][26];
+
+// memset(dp, -1, sizeof(dp));
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    // +2 -2 +3 -3
+
+    // if (n == 0)
+    // {
+    //     cout << 0;
+    //     return;
+    // }
+
+    if (n == 1)
+    {
+        // n%3==0;
+        cout << 2;
+    }
+    else
+    {
+        // n%3==1 need 2 more step
+        // n%3==2 need 1 more step
+        cout << (n + 2) / 3;
+    }
+}
+
 int main()
 {
-    int t;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 0;
+
     cin >> t;
 
-    for (int c = 0; c < t; ++c)
+    for (int i = 0; i < t; ++i)
     {
-
-        int n = 0;
-
-        cin >> n;
-
-        if (n == 1)
-        {
-            cout << 2;
-        }
-        else
-        {
-            cout << (n + 2) / 3;
-        }
+        solve();
 
         cout << endl;
     }
-
-    return 0;
 }
