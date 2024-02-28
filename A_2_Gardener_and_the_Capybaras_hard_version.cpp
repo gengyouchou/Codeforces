5dc17808f7646328769cf601261f90d5edc5c2a4
gengyou <resisttempetation23@gmail.com>
2024-02-28 22:02:05 +0800

    good constructive algorithms

--- A_2_Gardener_and_the_Capybaras_hard_version.cpp
+++ A_2_Gardener_and_the_Capybaras_hard_version.cpp
@@ -19,16 +19,27 @@
 #include <unordered_set>
 #include <vector>
 
+// setprecision example
+#include <iomanip> // std::setprecision
+
 using namespace std;
 
 using ll = long long;
 using ld = long double;
+using ull = unsigned long long;
 
-const ll N = 1'000'000'000'000L;
+const ll N = 1000000000000L;
 
 #define ab(x) (((x) < 0) ? -(x) : (x))
 
 ll M = 1e9 + 7;
+
+// int dp[1001][26][26];
+
+// memset(dp, -1, sizeof(dp));
+
+// A, B, C string consists only of a and b.
+// Either A≤B and C≤B, or B≤A and B≤C must be satisfied.
 
 void solve()
 {
@@ -37,20 +48,32 @@ void solve()
 
     int n = s.size();
 
-    string left = "", mid = "", right = "";
+    // left, a, right
+
+    // a must be the smaller one, which meet B≤A and B≤C
+
+    string left = " ", mid = " ", right = " ";
 
     for (int i = 1; i < n - 1; ++i)
     {
         if (s[i] == 'a')
         {
             left = s.substr(0, i);
-            mid = s.substr(i, 1);
+            mid = 'a';
             right = s.substr(i + 1);
 
-            cout << left << " " << mid << " " << right;
+            cout << left << " " << 'a' << " " << right;
+
             return;
         }
     }
+
+    // a, mid, b
+    // a, mid, a
+    // b, mid, a
+    // b, mid, b
+
+    // mid==bxxxx, which meet A≤B and C≤B
 
     mid = s.substr(1, n - 2);
 
